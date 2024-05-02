e22db1cf5df3e776ce7b8c10f33675e2b43478be
Enric Borrallo Rodriguez <borrallorodriguez@gmail.com>
2024-05-02 22:35:35 +0200

    Fix test auth firebase

--- src/Contexts/Auth/User/infrastructure/apis/LocalBlockchain.ts
+++ src/Contexts/Auth/User/infrastructure/apis/LocalBlockchain.ts
@@ -2,6 +2,6 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 
 export class LocalBlockchain {
   static get(): JsonRpcProvider {
-    return new JsonRpcProvider(`http://127.0.0.1:8545/`);
+    return new JsonRpcProvider(`http://localhost:8545/`);
   }
 }
