cb75b26dafa662316f3c4a150afaf65027127cbd
Enric Borrallo Rodriguez <borrallorodriguez@gmail.com>
2024-05-03 00:29:14 +0200

    fix

--- src/Contexts/Auth/User/infrastructure/apis/LocalBlockchain.ts
+++ src/Contexts/Auth/User/infrastructure/apis/LocalBlockchain.ts
@@ -2,7 +2,6 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 
 export class LocalBlockchain {
   static get(): JsonRpcProvider {
-    console.log('LocalBlockchain')
     return new JsonRpcProvider(`http://127.0.0.1:8545/`);
   }
 }
